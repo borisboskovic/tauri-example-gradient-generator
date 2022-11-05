ed3d747804700407339890ba90f26f14c468ee20
Boris Bošković <boris.boskovic92@gmail.com>
2022-11-05 23:21:46 +0100

    Added TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { SketchPicker, ColorChangeHandler } from "react-color";
 import { invoke } from "@tauri-apps/api/tauri";
-import classes from "./style.module.css";
 import Color from "color";
 
 function App() {
-    const [color, setColor] = useState("#00FF00");
-    const [colors, setColors] = useState<
-        { r: number; g: number; b: number }[] | undefined
-    >();
+  const [color, setColor] = useState("#00FF00");
+  const [colors, setColors] = useState<
+    { r: number; g: number; b: number }[] | undefined
+  >();
 
-    const colorChangeHandler: ColorChangeHandler = (color) => {
-        console.log("Change handler FE", color);
-        setColor(color.hex);
-        invoke<string>("generate_gradient", { ...color.rgb }).then(
-            (response) => {
-                const result = (
-                    response as unknown as [number, number, number][]
-                ).map((e) => ({ r: e[0], g: e[1], b: e[2] }));
-                setColors(result);
-            }
-        );
-    };
+  const colorChangeHandler: ColorChangeHandler = (color) => {
+    console.log("Change handler FE", color);
+    setColor(color.hex);
+    invoke<string>("generate_gradient", { ...color.rgb }).then((response) => {
+      const result = (response as unknown as [number, number, number][]).map(
+        (e) => ({ r: e[0], g: e[1], b: e[2] })
+      );
+      setColors(result);
+    });
+  };
 
-    return (
-        <>
-            <SketchPicker onChange={colorChangeHandler} color={color} />
-            <div className={classes.gradient_container}>
-                {colors &&
-                    colors.map((c) => (
-                        <div
-                            className={classes.gradient_slice}
-                            key={`${c.r}${c.g}${c.b}${Math.random()}`}
-                            style={{ backgroundColor: Color(c).hex() }}
-                        ></div>
-                    ))}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <SketchPicker onChange={colorChangeHandler} color={color} />
+      <div className="flex flex-col">
+        {colors &&
+          colors.map((c) => (
+            <div
+              className="grow h-4"
+              key={`${c.r}${c.g}${c.b}${Math.random()}`}
+              style={{ backgroundColor: Color(c).hex() }}
+            ></div>
+          ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
